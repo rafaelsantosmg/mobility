3d31c32de418b8749160b6a664e0bd5f1c7cc664
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-25 01:29:03 -0300

    refactor: ajuste na tela de home e novas implementações

--- app/frontend/src/pages/home/index.tsx
+++ app/frontend/src/pages/home/index.tsx
@@ -1,23 +1,77 @@
 import React from 'react';
+import useRide from '../../hooks/useRide';
 
 const Home: React.FC = () => {
+  const { onOriginChange, onDestinationChange, onEstimateRide } = useRide();
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6 w-full">
-      <div className='flex flex-col w-1/2'>
+    <div className="flex min-h-screen w-full flex-col items-center justify-start bg-gray-100 p-5 lg:pt-10">
+      <div className="mt-5 flex flex-col lg:w-3/4">
+        <h1 className="mb-4 text-2xl font-bold text-blue-600 lg:text-4xl">
+          Bem-vindo à Mobility
+        </h1>
+        <p className="text-lg text-gray-700">
+          Bem-vindo ao Mobility! O Mobility é a solução ideal para o transporte
+          particular, oferecendo praticidade e eficiência ao seu alcance. Com
+          uma interface simples e funcional, nossa plataforma foi desenvolvida
+          para proporcionar uma experiência de transporte tranquila e ágil,
+          conectando você aos melhores serviços de mobilidade. Nesta plataforma,
+          você poderá acessar facilmente informações sobre rotas, estimativas de
+          tempo e integração com o Google Maps, tudo para otimizar seu trajeto.
+          Seja para viagens curtas ou longas distâncias, o{' '}
+          <strong>Mobility</strong> está aqui para tornar o seu deslocamento
+          mais eficiente e seguro. Explore e aproveite o melhor da mobilidade!
+        </p>
+      </div>
 
-      <h1 className="text-4xl font-bold text-blue-600 mb-4">Bem-vindo à Mobility</h1>
-      <p className="text-lg text-gray-700 mb-4">
-      Bem-vindo ao Mobility!
+      <div className="mt-5 flex flex-col lg:w-3/4">
+        <p className="mb-4 text-lg text-gray-700">
+          Para iniciar informe o endereço de <strong>Origem</strong> e de{' '}
+          <strong>Destino</strong> depois clique em <strong>Calcular</strong>
+        </p>
+      </div>
 
-      O Mobility é a solução ideal para o transporte particular, oferecendo praticidade e eficiência ao seu alcance. Com uma interface simples e funcional, nossa plataforma foi desenvolvida para proporcionar uma experiência de transporte tranquila e ágil, conectando você aos melhores serviços de mobilidade.
+      <div className="mt-10 flex w-full flex-col gap-10 lg:w-3/4">
+        <div className="flex flex-col lg:justify-between">
+          <div className="mb-6 lg:w-3/4">
+            <label
+              htmlFor="large-input"
+              className="mb-2 block text-sm font-medium text-blue-600"
+            >
+              Endereço de Origem
+            </label>
+            <input
+              type="text"
+              id="large-input"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 text-base text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-200 dark:text-gray-600 dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              onChange={onOriginChange}
+            />
+          </div>
 
-      Nesta plataforma, você poderá acessar facilmente informações sobre rotas, estimativas de tempo e integração com o Google Maps, tudo para otimizar seu trajeto. Seja para viagens curtas ou longas distâncias, o <strong>Mobility</strong> está aqui para tornar o seu deslocamento mais eficiente e seguro.
+          <div className="mb-6 lg:w-3/4">
+            <label
+              htmlFor="large-input"
+              className="mb-2 block text-sm font-medium text-blue-600"
+            >
+              Endereço de Destino
+            </label>
+            <input
+              type="text"
+              id="large-input"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 text-base text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-200 dark:text-gray-600 dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              onChange={onDestinationChange}
+            />
+          </div>
+        </div>
 
-      Explore e aproveite o melhor da mobilidade!
-      </p>
-      <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-        Comece agora.
-      </button>
+        <div className="flex justify-end">
+          <button
+            className="h-12 w-40 rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+            onClick={onEstimateRide}
+          >
+            Calcular
+          </button>
+        </div>
       </div>
     </div>
   );
