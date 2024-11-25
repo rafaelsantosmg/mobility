19db2fa8272c9fb487909f83f49fa2fcaf6da570
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-25 01:33:00 -0300

    refactor: atualização para liberação de cors

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,19 +1,21 @@
-import cors from 'cors';
-import express from 'express';
-import handleError from './middlewares/handleError';
-import router from './routes/routes';
+import cors from 'cors'
+import express from 'express'
+import handleError from './middlewares/handleError'
+import router from './routes/routes'
 
-const app = express();
+const app = express()
 
-app.use(cors({
-  origin: ["http://localhost:80"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ['http://localhost', 'http://localhost:5173'],
+    credentials: true,
+  })
+)
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(router);
+app.use(router)
 
-app.use(handleError);
+app.use(handleError)
 
-export default app;
+export default app
