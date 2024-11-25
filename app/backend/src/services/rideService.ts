5176e015dfea5891674b1056dadfc413675da155
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-24 22:03:15 -0300

    feat: implementado a ride-service

---
+++ app/backend/src/services/rideService.ts
@@ -0,0 +1,41 @@
+import googleMapsClient from "../config/googleClient";
+import ServiceError from "../types/errors";
+import { calculateDrivers } from "../utils/calculateDrivers";
+
+export const calculateRideEstimate = async (origin: string, destination: string) => {
+  try {
+    const response = await googleMapsClient.directions({
+      params: {
+        origin,
+        destination,
+        key: process.env.GOOGLE_MAPS_API_KEY!,
+      },
+    });
+
+    const routeData = response.data;
+    if (!routeData.routes || routeData.routes.length === 0) {
+      throw new Error("Rota não encontrada.");
+    }
+
+    const route = routeData.routes[0];
+    const distanceMeters = route.legs[0].distance.value;
+    const distanceKm = distanceMeters / 1000;
+    const duration = route.legs[0].duration.text;
+    const startLocation = route.legs[0].start_location;
+    const endLocation = route.legs[0].end_location;
+
+    const drivers = calculateDrivers(distanceKm);
+
+    return {
+      origin: { latitude: startLocation.lat, longitude: startLocation.lng },
+      destination: { latitude: endLocation.lat, longitude: endLocation.lng },
+      distance: parseFloat(distanceKm.toFixed(2)),
+      duration,
+      options: drivers,
+      routeResponse: routeData,
+    };
+
+  } catch (error) {
+    throw new ServiceError("INVALID_DATA", "Erro ao calcular a rota. Verifique os dados fornecidos.");
+  }
+};
