9778f21d19e134206116869e0ffcd69c3a26d61a
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-24 22:05:10 -0300

    feat: configuração do prisma e dependencias

--- app/backend/prisma/seedUser.ts
+++ app/backend/prisma/seedUser.ts
@@ -6,15 +6,13 @@ async function main() {
   // Criação de usuários de exemplo
   const user1 = await prisma.user.create({
     data: {
-      email: "usuario1@teste.com",
-      password: "senha123",
+      name: "usuario1",
     },
   });
 
   const user2 = await prisma.user.create({
     data: {
-      email: "usuario2@teste.com",
-      password: "senha456",
+      email: "usuario2",
     },
   });
 
