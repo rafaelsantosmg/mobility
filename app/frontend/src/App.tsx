b06aab79cbca347a555cc5b1147e43a4e6da18df
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-25 01:28:01 -0300

    refactor: alterado a estrutura do app e o main para melhoria de legibilidade

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import Home from "./pages/home"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Fragment } from 'react/jsx-runtime';
+import AppRoutes from './router';
 
 function App() {
-
   return (
-    <Home />
-  )
+    <Fragment>
+      <ToastContainer />
+      <AppRoutes />
+    </Fragment>
+  );
 }
 
-export default App
+export default App;
