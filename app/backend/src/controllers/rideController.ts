84e22dc669563fa38497c702d3162553e97b17a9
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-25 01:35:45 -0300

    fix: correção do retorno da controller

--- app/backend/src/controllers/rideController.ts
+++ app/backend/src/controllers/rideController.ts
@@ -1,27 +1,44 @@
-import { Request, Response } from "express";
-import { calculateRideEstimate } from "../services/rideService";
-import ServiceError from "../types/errors";
+import { Request, Response } from 'express'
+import { calculateRideEstimate } from '../services/rideService'
+import ServiceError from '../types/errors'
 
-const estimateRideController = async (req: Request, res: Response): Promise<void> => {
+const estimateRideController = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
-    const { origin, destination } = req.body;
+    const { origin, destination } = req.body
 
-    const estimatedPrice = await calculateRideEstimate(origin, destination);
+    if (!origin || !destination) {
+      throw new ServiceError(
+        'INVALID_INPUT',
+        'Os dados de origem e destino são obrigatórios.'
+      )
+    }
 
-    res.status(200).json({ estimatedPrice });
+    if (origin === destination) {
+      throw new ServiceError(
+        'INVALID_DATA',
+        'O endereço de origem e destino não podem ser iguais.'
+      )
+    }
+
+    const estimatedPrice = await calculateRideEstimate(origin, destination)
+
+    res.status(200).json({ estimatedPrice })
   } catch (error) {
     if (error instanceof ServiceError) {
       res.status(400).json({
-        error_code: error.errorCode,
+        error_code: error.error_code,
         error_description: error.message,
-      });
+      })
+    } else {
+      res.status(500).json({
+        error_code: 'SERVER_ERROR',
+        error_description: 'Ocorreu um erro interno no servidor.',
+      })
     }
-
-    res.status(500).json({
-      error_code: "SERVER_ERROR",
-      error_description: "Ocorreu um erro interno no servidor.",
-    });
   }
-};
+}
 
-export default estimateRideController;
+export default estimateRideController
