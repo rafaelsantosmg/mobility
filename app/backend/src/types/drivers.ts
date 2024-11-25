e80cf53414327d2fd29a29bf8b441eaf1fbaec91
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-25 01:33:53 -0300

    feat: implementado types para o array de drivers

--- app/backend/src/types/drivers.ts
+++ app/backend/src/types/drivers.ts
@@ -1,14 +1,14 @@
 export type Review = {
-  rating: number;
-  comment: string;
+  rating: number
+  comment: string
 }
 
 export interface Driver {
-    id: number;
-    name: string;
-    description: string;
-    vehicle: string;
-    review: Review;
-    rate_per_km: number;
-    min_km: number;
+  id: number
+  name: string
+  description: string
+  vehicle: string
+  review: Review
+  rate_per_km: number
+  min_km: number
 }
