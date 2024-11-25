385b85c7b47cbcbda9c791979895c24d5755ade5
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-25 01:35:01 -0300

    refactor: refatoração para adicionar os types

--- app/backend/src/utils/calculateDrivers.ts
+++ app/backend/src/utils/calculateDrivers.ts
@@ -1,15 +1,13 @@
-import { Driver } from "../types/drivers";
-import { DRIVERS } from "./drivers";
+import { Driver } from '../types/drivers'
+import { DRIVERS } from './drivers'
 
 export const calculateDrivers = (distance: any) => {
-  return DRIVERS.filter((driver: Driver) => distance >= driver.minKm)
-    .map((driver) => ({
-      id: driver.id,
-      name: driver.name,
-      description: driver.description,
-      vehicle: driver.vehicle,
-      review: driver.review,
-      value: parseFloat((distance * driver.ratePerKm).toFixed(2)),
-    }))
-    .sort((a, b) => a.value - b.value);
-};
+  return DRIVERS.map((driver: Driver) => ({
+    id: driver.id,
+    name: driver.name,
+    description: driver.description,
+    vehicle: driver.vehicle,
+    review: driver.review,
+    value: parseFloat((distance * driver.rate_per_km).toFixed(2)),
+  })).sort((a, b) => a.value - b.value)
+}
