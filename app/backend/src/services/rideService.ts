e58a00fddce6e1d9d4b0fb748857cb6572f7b468
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-26 20:39:48 -0300

    refactor: refatoração da service para trabalhar com banco de dados

--- app/backend/src/services/rideService.ts
+++ app/backend/src/services/rideService.ts
@@ -1,13 +1,80 @@
+import { PrismaClient } from '@prisma/client'
+
+import { default as createServiceError } from '../Errors/errors'
 import googleMapsClient from '../config/googleClient'
-import ServiceError from '../types/errors'
-import { calculateDrivers } from '../utils/calculateDrivers'
+import { TDrivers, TDriversPrisma } from '../types/drivers'
+import { TRideData, TRideEstimateResponse } from '../types/rides'
+
+const prisma = new PrismaClient()
+
+const rideFindAll = async (): Promise<TDriversPrisma> => {
+  try {
+    const drivers = await prisma.driver.findMany({
+      include: {
+        review: {
+          select: {
+            rating: true,
+            comment: true,
+          },
+        },
+      },
+    })
+
+    return drivers.map((driver) => ({
+      ...driver,
+      review: {
+        rating: driver.review[0].rating,
+        comment: driver.review[0].comment,
+      },
+    }))
+  } catch (error) {
+    throw createServiceError('DRIVER_NOT_FOUND', 'Motoristas não encontrado')
+  }
+}
+
+const calculateDrivers = async (distance: number): Promise<TDrivers | []> => {
+  try {
+    const drivers: TDriversPrisma = await rideFindAll()
+
+    return drivers
+      .filter((driver) => distance >= driver.minKm)
+      .map((driver) => ({
+        id: driver.id,
+        name: driver.name,
+        description: driver.description,
+        vehicle: driver.vehicle,
+        review: driver.review,
+        value: parseFloat((distance * driver.ratePerKm).toFixed(2)),
+      }))
+      .sort((a, b) => a.value - b.value)
+  } catch {
+    throw createServiceError('DRIVER_NOT_FOUND', 'Motoristas não encontrado')
+  }
+}
+
+const formatResponseRideEstimate = (rideData: TRideData): TRideEstimateResponse => {
+  return {
+    origin: {
+      latitude: rideData.startLocation.lat,
+      longitude: rideData.startLocation.lng,
+    },
+    destination: {
+      latitude: rideData.endLocation.lat,
+      longitude: rideData.endLocation.lng,
+    },
+    distance: parseFloat(rideData.distanceKm.toFixed(2)),
+    duration: rideData.duration,
+    options: rideData.options,
+    routeResponse: rideData.routeReponse,
+  }
+}
 
 export const calculateRideEstimate = async (
   origin: string,
   destination: string
-) => {
+): Promise<TRideEstimateResponse> => {
   try {
-    const response = await googleMapsClient.directions({
+    const { data } = await googleMapsClient.directions({
       params: {
         origin,
         destination,
@@ -15,31 +82,28 @@ export const calculateRideEstimate = async (
       },
     })
 
-    const routeData = response.data
-    if (!routeData.routes || routeData.routes.length === 0) {
-      throw new Error('Rota não encontrada.')
-    }
-
-    const route = routeData.routes[0]
-    const distanceMeters = route.legs[0].distance.value
-    const distanceKm = distanceMeters / 1000
-    const duration = route.legs[0].duration.text
-    const startLocation = route.legs[0].start_location
-    const endLocation = route.legs[0].end_location
+    const route = data.routes[0]
+    const distanceKm = route.legs[0].distance.value / 1000
 
     const drivers = await calculateDrivers(distanceKm)
 
-    return {
-      origin: { latitude: startLocation.lat, longitude: startLocation.lng },
-      destination: { latitude: endLocation.lat, longitude: endLocation.lng },
-      distance: parseFloat(distanceKm.toFixed(2)),
-      duration,
+    const rideData: TRideData = {
+      distanceMeters: route.legs[0].distance.value,
+      distanceKm,
+      duration: route.legs[0].duration.text,
+      startLocation: route.legs[0].start_location,
       options: drivers,
-      routeResponse: routeData,
+      endLocation: route.legs[0].end_location,
+      routeReponse: data,
     }
+
+    const formatedResponseRide: TRideEstimateResponse =
+      formatResponseRideEstimate(rideData)
+
+    return formatedResponseRide
   } catch (error) {
-    throw new ServiceError(
-      'INVALID_DATA',
+    throw createServiceError(
+      'INVALID_ROUTE',
       'Erro ao calcular a rota. Verifique os dados fornecidos.'
     )
   }
