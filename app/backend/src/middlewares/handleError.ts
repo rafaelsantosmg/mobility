c23ae5bc85edf936955ad4212ae3e7a9c51b2367
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-26 20:38:22 -0300

    refactor: refatoração do middleware de erro

--- app/backend/src/middlewares/handleError.ts
+++ app/backend/src/middlewares/handleError.ts
@@ -1,5 +1,8 @@
-import { NextFunction, Request, Response } from 'express';
-import { ErrorWithStatus } from '../types/errors';
+import { NextFunction, Request, Response } from 'express'
+
+interface ErrorWithStatus extends Error {
+  status?: number
+}
 
 function handleError(
   err: ErrorWithStatus,
@@ -8,9 +11,9 @@ function handleError(
   _next: NextFunction
 ): void {
   if (err.status) {
-    res.status(err.status).json({ message: err.message });
+    res.status(err.status).json({ message: err.message })
   }
-  res.status(500).json({ message: 'Internal Error' });
-};
+  res.status(500).json({ message: 'Internal Error' })
+}
 
-export default handleError;
+export default handleError
