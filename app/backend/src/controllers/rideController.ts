262e87e32a83ac4d93f71093bd23be5dd7aa63aa
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-27 10:44:05 -0300

    feat: implementado funcionalidade da rota de confirmação para a controller

--- app/backend/src/controllers/rideController.ts
+++ app/backend/src/controllers/rideController.ts
@@ -1,42 +1,38 @@
 import { Request, Response } from 'express'
 
-import { default as createServiceError, isServiceError } from '../Errors/errors'
-import { calculateRideEstimate } from '../services/rideService'
+import { isServiceError } from '../Errors/errors'
+import { calculateRideEstimate, rideCreateHistory } from '../services/rideService'
 
-const estimateRideController = async (req: Request, res: Response): Promise<void> => {
+export const rideEstimateController = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
-    const { origin, destination } = req.body
+    const response = await calculateRideEstimate(req.body)
 
-    if (!origin || !destination) {
-      throw createServiceError(
-        'INVALID_INPUT',
-        'Os dados de origem e destino são obrigatórios.'
-      )
-    }
-
-    if (origin === destination) {
-      throw createServiceError(
-        'INVALID_DATA',
-        'O endereço de origem e destino não podem ser iguais.'
-      )
-    }
-
-    const estimatedPrice = await calculateRideEstimate(origin, destination)
-
-    res.status(200).json({ estimatedPrice })
+    res.status(200).json(response)
   } catch (error) {
     if (isServiceError(error)) {
-      res.status(400).json({
-        error_code: error.error_code,
-        error_description: error.message,
-      })
-    } else {
-      res.status(500).json({
-        error_code: 'SERVER_ERROR',
-        error_description: 'Ocorreu um erro interno no servidor.',
+      res.status(404).json({
+        error_code: 'NOT_FOUND',
+        error_description: 'Não foi encontrado um ride com esse ID.',
       })
     }
   }
 }
 
-export default estimateRideController
+export const rideConfirmController = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const response = await rideCreateHistory(req.body)
+
+    res.status(200).json(response)
+  } catch (error) {
+    res.status(404).json({
+      error_code: 'NOT_FOUND',
+      error_description: 'Não foi encontrado um ride com esse ID.',
+    })
+  }
+}
