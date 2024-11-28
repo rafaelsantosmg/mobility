97713e5af461437219ceef19bc51c2940cbd098a
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-27 23:31:24 -0300

    style: correções de estilos

--- app/frontend/src/components/Estimated.tsx
+++ app/frontend/src/components/Estimated.tsx
@@ -1,44 +1,29 @@
-import { useMemo, useState } from 'react';
 import { Fragment } from 'react/jsx-runtime';
 import { useAppContext } from '../hooks/useAppContext';
 import useRide from '../hooks/useRide';
 import { DriverOption } from '../interfaces/googleClient';
 import { ConfirmRidePayload } from '../interfaces/ride';
 import convertCurrencyPtBr from '../utils/convertCurrencyPtBr';
+import { formatDuration } from '../utils/formatDuration';
 import LoaderSpinner from './LoaderSpinner';
 import ModalConfirm from './ModalConfirm';
 
 const Estimated: React.FC = () => {
-  const { estimatedRide } = useAppContext();
-  const { onConfirmRide } = useRide();
-  const [driver, setDriver] = useState<DriverOption | null>(null);
+  const { estimatedRide, customerId } = useAppContext();
+  const { onConfirmRide, loading } = useRide();
 
-  const handleSelectDriver = (driver: DriverOption) => {
-    setDriver(driver);
-  };
-
-  const travel = useMemo(() => {
-    return {
+  const handleSubmit = (driver: DriverOption) => {
+    const payload: ConfirmRidePayload = {
+      customer_id: customerId ? customerId : '1',
       distance: estimatedRide?.distance,
       duration: estimatedRide?.duration,
       origin: estimatedRide?.routeResponse?.routes[0].legs[0].start_address,
       destination: estimatedRide?.routeResponse?.routes[0].legs[0].end_address,
-      driver: driver,
-    };
-  }, [estimatedRide, driver]);
-
-  const handleSubmit = () => {
-    const payload: ConfirmRidePayload = {
-      customer_id: '1',
-      origin: travel.origin ?? '',
-      destination: travel.destination ?? '',
-      distance: travel.distance ?? 0,
-      duration: travel.duration ?? '',
       driver: {
-        id: travel.driver?.id ?? '',
-        name: travel.driver?.name ?? '',
+        id: driver.id,
+        name: driver.name,
       },
-      value: travel.driver?.value ?? 0,
+      value: driver.value,
     };
     onConfirmRide(payload);
   };
@@ -47,62 +32,71 @@ const Estimated: React.FC = () => {
     <LoaderSpinner />
   ) : (
     <Fragment>
-      <div className="flex w-full flex-col gap-3 pl-5">
+      <div className="mt-10 flex w-full flex-col gap-3 lg:mt-0 lg:pl-5">
         <p className="text-xl font-bold text-gray-700">
-          Distancia: {estimatedRide.distance} Km
+          Distancia: {estimatedRide.distance.toFixed(2)} Km
         </p>
         <p className="text-xl font-bold text-gray-700">
-          Duração estimada: {estimatedRide.duration.slice(0, 6)}
+          Duração estimada: {formatDuration(estimatedRide.duration)}
         </p>
-
-        {estimatedRide.options.length > 0 ? (
-          estimatedRide.options.map((option: DriverOption) => (
-            <button
-              type="button"
-              key={option.id}
-              className="flex flex-col items-start justify-center gap-1 rounded-md border border-gray-300 p-2 hover:bg-gray-300"
-              onClick={() => handleSelectDriver(option)}
-            >
-              <p className="text-left text-lg font-semibold text-blue-600">
-                {option.name}
-              </p>
-              <p className="text-left text-gray-700">{option.description}</p>
-              <div className="flex flex-col items-start justify-center gap-1">
-                <p className="text-left text-gray-700">
-                  <strong>Avaliçao: </strong>
-                  {Array.from({ length: 5 }, (_, index) =>
-                    index < (option.review.rating ?? 0) ? '⭐' : '☆'
-                  ).join('')}
+        <div className="flex h-screen flex-col gap-3 overflow-auto lg:h-[65vh]">
+          <p className="text-sm font-semibold">
+            * Selecione um motorista para confirmar a viagem.
+          </p>
+          {estimatedRide.options.length > 0 ? (
+            estimatedRide.options.map((option: DriverOption) => (
+              <div
+                key={option.id}
+                className="flex flex-col items-start justify-center gap-1 rounded-md border border-gray-300 p-2"
+              >
+                <p className="text-left text-lg font-semibold text-blue-600">
+                  {option.name}
                 </p>
-
+                <p className="text-left text-gray-700">{option.description}</p>
                 <p className="text-left text-gray-700">
-                  {option.review?.comment}
+                  <strong>Veículo: </strong>
+                  {option.vehicle}
                 </p>
+                <div className="flex flex-col items-start justify-center gap-1">
+                  <p className="text-left text-gray-700">
+                    <strong>Avaliçao: </strong>
+                    {Array.from({ length: 5 }, (_, index) =>
+                      index < (option.review.rating ?? 0) ? '⭐' : '☆'
+                    ).join('')}
+                  </p>
+
+                  <p className="text-left text-gray-700">
+                    {option.review?.comment}
+                  </p>
+                </div>
+                <div className="flex w-full flex-row items-center justify-between">
+                  <p className="text-left text-gray-700">
+                    <strong>Valor:</strong> {convertCurrencyPtBr(option.value)}
+                  </p>
+                  <button
+                    className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+                    onClick={() => handleSubmit(option)}
+                  >
+                    Escolher
+                  </button>
+                </div>
               </div>
-              <p className="text-left text-gray-700">
-                <strong>Valor:</strong> {convertCurrencyPtBr(option.value)}
+            ))
+          ) : (
+            <div className="mt-5 flex flex-col items-start justify-center gap-1">
+              {' '}
+              <p className="text-left text-2xl font-semibold text-blue-600">
+                Não há motoristas disponíveis para esta rota.
               </p>
-            </button>
-          ))
-        ) : (
-          <div className="mt-5 flex flex-col items-start justify-center gap-1">
-            {' '}
-            <p className="text-left text-2xl font-semibold text-blue-600">
-              Não há motoristas disponíveis para esta rota.
-            </p>
-            <p className="text-left text-gray-700">
-              Por favor, escolha outra rota.
-            </p>
-          </div>
-        )}
+              <p className="text-left text-gray-700">
+                Por favor, escolha outra rota.
+              </p>
+            </div>
+          )}
+        </div>
       </div>
 
-      <ModalConfirm
-        isOpen={!!driver?.id}
-        onClose={() => setDriver(null)}
-        travel={travel}
-        onSubmit={handleSubmit}
-      />
+      <ModalConfirm isOpen={loading} />
     </Fragment>
   );
 };
--- app/frontend/src/components/GoogleMaps.tsx
+++ app/frontend/src/components/GoogleMaps.tsx
@@ -1,9 +1,4 @@
-import {
-  DirectionsRenderer,
-  GoogleMap,
-  Marker,
-  useLoadScript,
-} from '@react-google-maps/api';
+import { DirectionsRenderer, GoogleMap } from '@react-google-maps/api';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { useAppContext } from '../hooks/useAppContext';
@@ -12,34 +7,14 @@ const isMobile = window.innerWidth <= 768;
 
 const containerStyle = {
   width: '100%',
-  height: isMobile ? '500px' : '100%',
+  height: isMobile ? '500px' : '95%',
 };
 
 const GoogleMaps: React.FC = () => {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-  });
   const { originRequest, destinationRequest } = useAppContext();
-  const [currentLocation, setCurrentLocation] =
-    useState<google.maps.LatLngLiteral | null>(null);
+
   const [directionsResponse, setDirectionsResponse] =
     useState<google.maps.DirectionsResult | null>(null);
-
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setCurrentLocation({ lat: latitude, lng: longitude });
-        },
-        (error) => {
-          console.error('Erro ao obter localização:', error);
-        }
-      );
-    } else {
-      toast.error('Geolocalização não é suportada pelo navegador.');
-    }
-  }, []);
 
   useEffect(() => {
     if (originRequest && destinationRequest) {
@@ -63,20 +38,13 @@ const GoogleMaps: React.FC = () => {
     }
   }, [originRequest, destinationRequest]);
 
-  if (loadError) return <div>Erro ao carregar o mapa</div>;
-  if (!isLoaded) return <div>Carregando...</div>;
-
   return (
     <div className="flex w-full flex-col">
       <GoogleMap
         mapContainerStyle={containerStyle}
-        center={currentLocation || { lat: 0, lng: 0 }}
-        zoom={currentLocation ? 15 : 2}
+        center={{ lat: -23.533773, lng: -46.62529 }} // latitude and longitude of São Paulo
+        zoom={15}
       >
-        {/* Marcador na localização atual */}
-        {!directionsResponse && currentLocation && (
-          <Marker position={currentLocation} />
-        )}
         {/* Renderizador das rotas */}
         {directionsResponse && (
           <DirectionsRenderer directions={directionsResponse} />
