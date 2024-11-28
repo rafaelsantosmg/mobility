45a6ddea9547005f2fceace44db8a92860b452da
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-27 23:27:07 -0300

    refactor: adcionado rota de customer

--- app/backend/src/routes/rideRoute.ts
+++ app/backend/src/routes/rideRoute.ts
@@ -1,23 +1,23 @@
 import { Router } from 'express'
 
-import {
-  rideConfirmController,
-  rideEstimateController,
-} from '../controllers/rideController'
+import RideController from '../controllers/rideController'
 import validateRideRequest from '../middlewares/validateRideRequest'
 import rideEstimateSchema, { rideConfirmSchema } from '../schemas/rideSchemas'
 
 const rideRouter = Router()
+const rideController = RideController()
+
+rideRouter.get('/:customer_id', rideController.rideGetHistoryController)
 
 rideRouter.post(
   '/estimate',
   validateRideRequest(rideEstimateSchema),
-  rideEstimateController
+  rideController.rideEstimateController
 )
 rideRouter.patch(
   '/confirm',
   validateRideRequest(rideConfirmSchema),
-  rideConfirmController
+  rideController.rideConfirmController
 )
 
 export default rideRouter
