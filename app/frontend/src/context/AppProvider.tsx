ef59b81511ca2e05c6e0908d53493cac6d4d8c6f
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2024-11-27 23:31:56 -0300

    refactor: adicionado novos estados ao contexto

--- app/frontend/src/context/AppProvider.tsx
+++ app/frontend/src/context/AppProvider.tsx
@@ -2,6 +2,7 @@ import React, { createContext, ReactNode, useEffect, useState } from 'react';
 import {
   DestinationRequest,
   EstimatedRide,
+  HistoryRide,
   OriginRequest,
 } from '../interfaces/googleClient';
 
@@ -18,6 +19,14 @@ export interface AppContextType {
   setDestinationRequest: React.Dispatch<
     React.SetStateAction<DestinationRequest | undefined>
   >;
+  customerId: string | null;
+  setCustomerId: React.Dispatch<React.SetStateAction<string | null>>;
+  historyRide: HistoryRide[] | undefined;
+  setHistoryRide: React.Dispatch<
+    React.SetStateAction<HistoryRide[] | undefined>
+  >;
+  historyRideDriver: HistoryRide[] | undefined;
+  setHistoryRideDriver: React.Dispatch<HistoryRide[] | undefined>;
 }
 
 export const AppContext = createContext<AppContextType | undefined>(undefined);
@@ -27,15 +36,22 @@ interface AppProviderProps {
 }
 
 export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
+  const [customerId, setCustomerId] = useState<string | null>(null);
   const [estimatedRide, setEstimatedRide] = useState<EstimatedRide>();
   const [originRequest, setOriginRequest] = useState<OriginRequest>();
+  const [historyRide, setHistoryRide] = useState<HistoryRide[]>();
+  const [historyRideDriver, setHistoryRideDriver] = useState<HistoryRide[]>();
   const [destinationRequest, setDestinationRequest] =
     useState<DestinationRequest>();
 
   useEffect(() => {
     const savedPrice = localStorage.getItem('estimatedRide');
+    const saveCustomerId = localStorage.getItem('customerId');
     if (savedPrice) {
       setEstimatedRide(JSON.parse(savedPrice));
+    }
+    if (saveCustomerId) {
+      setCustomerId(saveCustomerId);
     }
   }, []);
 
@@ -44,6 +60,12 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
       localStorage.setItem('estimatedRide', JSON.stringify(estimatedRide));
     }
   }, [estimatedRide]);
+
+  useEffect(() => {
+    if (customerId) {
+      localStorage.setItem('customerId', customerId);
+    }
+  }, [customerId]);
 
   return (
     <AppContext.Provider
@@ -54,6 +76,12 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
         setOriginRequest,
         destinationRequest,
         setDestinationRequest,
+        customerId,
+        setCustomerId,
+        historyRide,
+        setHistoryRide,
+        historyRideDriver,
+        setHistoryRideDriver,
       }}
     >
       {children}
